25572581931ef1ebd7a8515fbffdae088d45f75b
Augustin Jose <44978968+AugustinJose1221@users.noreply.github.com>
2020-10-18 15:54:11 +0530

    Update hexToImage.py

--- src/python_files/hexToImage.py
+++ src/python_files/hexToImage.py
@@ -5,15 +5,7 @@ content = File.read()
 val = 0
 l = []
 for i in content.split("\n"):
-    '''
-    if i=='':
-        continue
-    k = i.split("x")[1]
-    if len(k)==1:
-        k = "0" + k
-    '''
     val = int(i, 16)
     l.append(val)
-#print(len(content.split("\n")))
 bgrImage = numpy.array(l).reshape(64, 64)
 cv2.imwrite('../../res/img/Foo.jpg', bgrImage)
